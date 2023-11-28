3c476c58f004b8042c8dcacc4836b06be74b8123
Dungeon_M. <novokhatnii.aa@phystech.edu>
2023-11-29 00:07:39 +0300

    Fixed a bug with size of simulation and collision with walls in topple_neumann

--- sand_model.py
+++ sand_model.py
@@ -6,10 +6,9 @@ from sandpile_constants import *
 
 def simulation(simulation_width, simulation_height, topple_type):
     screen = pygame.display.set_mode((simulation_width, simulation_height), pygame.SCALED|pygame.RESIZABLE)
-    print(pygame.SCALED)
 
     #двумерный массив для записи количества песчтнок в клетке
-    sandpiles = np.zeros((simulation_width, simulation_height), dtype=np.uint32)
+    sandpiles = np.zeros((simulation_height, simulation_width), dtype=np.uint32)
 
     #помещаем в центральную клетку много песчинок
     sandpiles[simulation_height // 2][simulation_width // 2] = 3e4
@@ -29,7 +28,7 @@ def simulation(simulation_width, simulation_height, topple_type):
         pygame.display.update()
 
         #создаём массив, в который будем записывать состояние после рассыпания; нужен для симметричного процесса отрисоки
-        nextsandpiles = np.zeros((simulation_width, simulation_height), dtype=np.uint32)
+        nextsandpiles = np.zeros((simulation_height, simulation_width), dtype=np.uint32)
 
         for i in range(simulation_height):
             for j in range(simulation_width):
@@ -37,7 +36,7 @@ def simulation(simulation_width, simulation_height, topple_type):
                 if num < N:
                     nextsandpiles[i][j] += num
                 else:
-                    set_topple_function(nextsandpiles, i, j, num, topple_type)
+                    set_topple_function(nextsandpiles, i, j, num, topple_type, simulation_width, simulation_height)
 
         sandpiles = nextsandpiles
 
@@ -47,5 +46,5 @@ def simulation(simulation_width, simulation_height, topple_type):
             if event.type == pygame.MOUSEBUTTONUP:
                 x, y = pygame.mouse.get_pos()
                 put_sand(sandpiles, x, y, 1e3)
-                print(x, y)
+
     pygame.quit()
